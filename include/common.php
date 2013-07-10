01b0fdf77324cc7fe5ea8028634c85e0f04a4cd3
Joey <joey.v.q@gmail.com>
2013-07-10 00:57:31 -0400

    Added magic quotes detection/removal

--- include/common.php
+++ include/common.php
@@ -4,6 +4,37 @@
 require_once('config.php');
 
 
+
+
+if (get_magic_quotes_gpc()) {
+	function undoMagicQuotes($array, $topLevel=true) {
+		
+		$newArray = array();
+		
+		foreach($array as $key => $value) {
+			if (!$topLevel) {
+				$key = stripslashes($key);
+				}
+			if (is_array($value)) {
+				$newArray[$key] = undoMagicQuotes($value, false);
+			}else {
+				$newArray[$key] = stripslashes($value);
+				}
+			}
+		
+		return $newArray;
+		}
+	
+    $_GET = undoMagicQuotes($_GET);
+    $_POST = undoMagicQuotes($_POST);
+    $_COOKIE = undoMagicQuotes($_COOKIE);
+    $_REQUEST = undoMagicQuotes($_REQUEST);
+	}
+
+
+
+
+
 if(!isset($storage)){
 	$storage = 'flatfile';
 	}
